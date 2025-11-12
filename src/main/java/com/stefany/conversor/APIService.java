2b42a8c56121bc298877a7cec6473293dc7e1b44
Stéfany de Oliveira Teixeira <stefanyoliveira.teixeira@estudante.ufjf.br>
2025-11-12 16:50:20 -0300

    Conversor

--- src/main/java/com/stefany/conversor/APIService.java
+++ src/main/java/com/stefany/conversor/APIService.java
@@ -11,9 +11,9 @@ public class APIService {
     private static final String BASE_URL = "https://v6.exchangerate-api.com/v6";
     private static final String API_KEY = "4e2edb166f4e051bd9ee44a2 ";
 
-    public JsonObject buscarTaxa (String moedaOrigen, String moedaDestino)
+    public JsonObject buscarTaxa (String moedaOrigem, String moedaDestino)
         throws IOException, InterruptedException {
-        String url = BASE_URL + API_KEY + "/pair/" + moedaOrigen + "/" + moedaDestino;
+        String url = BASE_URL + API_KEY + "/pair/" + moedaOrigem + "/" + moedaDestino;
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(URI.create(url)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
--- src/main/java/com/stefany/conversor/Conversor.java
+++ src/main/java/com/stefany/conversor/Conversor.java
@@ -1,4 +1,23 @@
 package com.stefany.conversor;
+import com.google.gson.JsonObject;
+import java.io.IOException;
 
 public class Conversor {
+    private final APIService apiService = new APIService();
+
+    public double converter (double valor, String moedaOrigem, String moedaDestino){
+        try {
+            JsonObject json = apiService.buscarTaxa(moedaOrigem, moedaDestino);
+            if (json.has("conversion_rate")) {
+                double taxa = json.get("conversion_rate").getAsDouble();
+                return valor * taxa;
+            } else {
+                System.out.println("Erro: respota inválida da API");
+                return 0.0;
+            }
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Erro ao conectar ao API" + e.getMessage());
+            return 0.0;
+        }
+    }
 }
